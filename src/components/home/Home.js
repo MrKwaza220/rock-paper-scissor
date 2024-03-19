e5a3fc5ab72bffdebe5108c051ddb9b8454812f3
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-03-19 20:17:54 +0200

    added components

---
+++ src/components/home/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <section id="home">
+      <div className="container">
+        <h1>ROCK PAPER SCISSOR GAME</h1>
+
+        <div className="home_page">
+            
+
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
