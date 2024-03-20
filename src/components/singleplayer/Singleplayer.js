80ec395322b75e47cfa46462ca555802e826dbd7
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-03-20 23:13:18 +0200

    added html file

--- src/components/singleplayer/Singleplayer.js
+++ src/components/singleplayer/Singleplayer.js
@@ -1,11 +1,54 @@
 import React from "react";
+import "./Singleplayer.css";
 
-const Singleplayer = () =>{
-    return(
-        <section id="single_player">
+const Singleplayer = () => {
+  return (
+    <section id="single_player">
+      <div className="container">
+        <div className="result_field">
+          <div className="score_result">
+            <div>
+              User Score
+              <span className="user_score"></span>
+            </div>
 
-        </section>
-    )
-}
+            <div>
+              <span className="cpu_score"> </span>
+              CPU
+            </div>
+          </div>
+          <div className="result_images">
+            <span className="user_result">
+              <image src="rock.jpg" alt="" />
+            </span>
+            <span className="cpu_result">
+              <image src="rock.jpg" alt="" />
+            </span>
+          </div>
+          <div className="result">Let's play</div>
+        </div>
+
+
+        {/*********** * Image********** */}
+        <div className="option_images">
+          <span className="option_image">
+            <img src="rock.jpg" alt="" />
+            <p>Rock</p>
+          </span>
+
+          <span className="option_image">
+            <img src="paper.jpg" alt="" />
+            <p>Paper</p>
+          </span>
+
+          <span className="option_image">
+            <img src="scissors.jpg" alt="" />
+            <p>Scissors</p>
+          </span>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Singleplayer;
