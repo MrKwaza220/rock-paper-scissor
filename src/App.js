26a41df5f41f4d042bb08cbede80d1df04a5e7b4
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-03-20 20:25:13 +0200

    added router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/home/Home';
+import Singleplayer from './components/singleplayer/Singleplayer';
 
 function App() {
   return (
-    <div className="App">
-       <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Add more routes here */}
+      </Routes>
+    </Router>
   );
 }
 
