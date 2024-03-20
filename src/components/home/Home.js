44b0d9408c915e0ba8fa4261368c217a407fb7dc
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-03-20 21:16:05 +0200

    aligned heading h1 center

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,7 +8,6 @@ function Home() {
       <div className="home_background">
         <div className="container">
           <h1>ROCK PAPER SCISSOR GAME</h1>
-          <p>Let's play</p>
 
           <div className="home_page">
           
