cfbed7fddf0a1c9b1a5ed3cbffec56fc4b5414e2
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-03-28 21:55:47 +0200

    added multiplayer files

---
+++ src/components/multiplayer/Multiplayer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Multiplplayer.css";
+
+const Multiplplayer =() =>{
+    return(
+       <section id="multiplayer">
+        <h1>multiplayer Game</h1>
+       </section> 
+    )
+}
+
+export default Multiplplayer;
