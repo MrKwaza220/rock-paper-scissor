f36cd5a11d0b3c58e02e8c215f162811c9b3afc4
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-04-05 21:20:12 +0200

    added creategame file to app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './components/home/Home';
 import Singleplayer from './components/singleplayer/Singleplayer';
 import Multiplplayer from './components/multiplayer/Multiplayer';
+import Creategame from './components/creategame/Creategame';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/single_player" element={<Singleplayer />} />
         <Route path="/multiplayer" element={<Multiplplayer></Multiplplayer>} />
+        <Route path="/create_multiplayer" element={<Creategame></Creategame>} />
       </Routes>
     </Router>
   );
