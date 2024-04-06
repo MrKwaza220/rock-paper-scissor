063652764855250bf378515ead09ace67c496f42
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-04-06 14:01:35 +0200

    added new class

--- src/components/creategame/Creategame.js
+++ src/components/creategame/Creategame.js
@@ -10,34 +10,35 @@ const Creategame = () => {
       <div className="container">
         <h1 className="create_title">ROCK PAPER & SCISSORS GAME</h1>
         <div className="create_game">
-
-            {/* Images */}
+          {/*  Images */}
           <div className="create_images">
             <img src={rockImage} alt="Rock" />
             <img src={paperImage} alt="Paper" />
             <img src={scissorImage} alt="Scissor" />
           </div>
-           
-           {/**** create button ****/}
-          <button
-            className="create_button creategame_btn"
-            onclick="createGame()"
-          >
-            Create Multiplayer Game
-          </button>
-            
-          {/* input field */}
-          <input
-            className="enter_code"
-            placeholder="Enter Code here"
-            type="text"
-            name=""
-            id="roomUniqueId"
-          />
-          {/*Joinning button */}
-          <button className="join_button creategame_btn" onclick="joinGame()">
-            Join Game
-          </button>
+
+          {/**** create button ****/}
+          <div className="button_input">
+            <button
+              className="create_button creategame_btn"
+              onclick="createGame()"
+            >
+              Create Multiplayer Game
+            </button>
+
+            {/* input field */}
+            <input
+              className="enter_code"
+              placeholder="Enter Code here"
+              type="text"
+              name=""
+              id="roomUniqueId"
+            />
+            {/*Joinning button */}
+            <button className="join_button creategame_btn" onclick="joinGame()">
+              Join Game
+            </button>
+          </div>
         </div>
       </div>
     </section>
